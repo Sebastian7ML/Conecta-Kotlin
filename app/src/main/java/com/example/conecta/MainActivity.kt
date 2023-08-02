9abf74cf6639bfbf167fcc6a8fcffe04e66f6f9e
Sebastian7ML <sebastian7ml@gmail.com>
2023-08-01 22:37:34 -0500

    lesson one design v0.1

--- app/src/main/java/com/example/conecta/MainActivity.kt
+++ app/src/main/java/com/example/conecta/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.conecta
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.example.conecta.lesson1.LessonOne
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById<Button>(R.id.btLetsLearn)
+        btnStart.setOnClickListener {
+
+            navigateToTutorial()
+            Log.i("s7", "starting tutorial")
+        }
+
+    }
+
+    private fun navigateToTutorial() {
+        val intent = Intent(this, LessonOne::class.java)
+        startActivity(intent)
     }
 }
