644fc0d290edf30aacc065e0df37fa560b7cecf0
Sebastian7ML <sebastian7ml@gmail.com>
2023-08-15 22:49:50 -0500

    added cards to lesson2

---
+++ app/src/main/java/com/example/conecta/lesson2/LessonTwo.kt
@@ -0,0 +1,12 @@
+package com.example.conecta.lesson2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.conecta.R
+
+class LessonTwo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lesson_two)
+    }
+}
